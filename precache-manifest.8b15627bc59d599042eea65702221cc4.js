1e7689bd80a0ffdf7c6735c7f7f75f4f0dc979de
s777610 <s777610@gmail.com>
2019-04-24 15:14:44 -0700

    Updates

--- precache-manifest.8b15627bc59d599042eea65702221cc4.js
+++ precache-manifest.8b15627bc59d599042eea65702221cc4.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/weicheng-portfolio/static/media/project6.a6f34174.png"
   },
   {
-    "revision": "d93dcd9a990788e22558",
+    "revision": "8656235c3b808a9fe515",
     "url": "/weicheng-portfolio/static/css/main.028716c3.chunk.css"
   },
   {
@@ -44,8 +44,8 @@ self.__precacheManifest = [
     "url": "/weicheng-portfolio/static/media/project5.d214d5e9.png"
   },
   {
-    "revision": "d93dcd9a990788e22558",
-    "url": "/weicheng-portfolio/static/js/main.f7a151eb.chunk.js"
+    "revision": "8656235c3b808a9fe515",
+    "url": "/weicheng-portfolio/static/js/main.4762adaf.chunk.js"
   },
   {
     "revision": "c7af9b46bfac25183e245d2e386830a8",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/weicheng-portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
   },
   {
-    "revision": "8a54b863c973d5466c2ceb4d5aa98f3a",
+    "revision": "dc97a19a9b05ad3f6bb6cf8647079175",
     "url": "/weicheng-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weicheng-portfolio/precache-manifest.ccb60ef28b39fbc6bb50a358af919db7.js"
+  "/weicheng-portfolio/precache-manifest.8b15627bc59d599042eea65702221cc4.js"
 );
 
 workbox.clientsClaim();
